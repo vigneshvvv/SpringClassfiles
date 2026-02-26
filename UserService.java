e5bbb5684451cd5cc37f0c1c1d86677719079010
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-02-26 11:46:54 +0530

    Implement UserService with sample user details method

---
+++ UserService.java
@@ -0,0 +1,18 @@
+package com.sample.demo.service;
+
+import com.sample.demo.model.UserDetails;
+
+import org.springframework.stereotype.Service;
+
+@Service no usages
+
+public class UserService (
+
+public UserDetails getUserDetailsSample(){ no usages
+
+UserDetails ugong UserDetails.builder().id(1).firstName("kumar").lastName("M").build();
+
+return users;
+
+}
+}
