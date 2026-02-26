e85c4fc97f0a52920d4795bcd26b054bf8db70a8
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-02-26 11:47:58 +0530

    Create UserDetails class with Lombok annotations

---
+++ UserDetails.java
@@ -0,0 +1,27 @@
+package com.sample.demo.model;
+
+import lombok.*;
+
+@Getter 3 usages
+
+@Setter
+
+@NoArgsConstructor
+
+@LLArgsConstructor
+
+@Builder
+
+public class UserDetails {
+
+private int id;
+
+private String firstName;
+
+private String lastName;
+
+private String mobile;
+
+private String email;
+
+}
