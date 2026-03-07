5edd3a89aea4a496bcfd2b67896dc247990b16de
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-03-07 23:47:02 +0530

    Add user data handling methods in UserService

--- UserService.java
+++ UserService.java
@@ -15,4 +15,31 @@ UserDetails ugong UserDetails.builder().id(1).firstName("kumar").lastName("M").b
 return users;
 
 }
+public UserDetails saveUserData(UserDetails userDetails){ 1 usage return userDetailsRepository.save(userDetails);
+
+}
+
+public Optional<UserDetails>> getByIDExample (Integer id) { 1 usage
+
+return userDetailsRepository.findById(id);
+
+}
+
+public Optional<UserDetails> getByUSerName(String userName) ( 1 usage
+
+return userDetailsRepository.FindByFirstNameNew(userName);
+
+}
+
+public List<UserDetails> getBYAge(int age){ 1 usage return userDetailsRepository.FindByAge(age);
+
+}
+
+public Users getUser(){ no usages
+
+Users user Users.builder().id(1).firstName("kumar").lastName("").age(10).email("v@gmail.com").birthDate("1/1/21").build();
+
+return useri;
+
+}
 }
