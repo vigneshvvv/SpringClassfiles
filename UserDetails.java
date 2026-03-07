eda0141dd0f14298936db0bbe433ff1e8ec58372
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-03-07 23:52:30 +0530

    Refactor UserDetails class with validation annotations

--- UserDetails.java
+++ UserDetails.java
@@ -2,26 +2,43 @@ package com.sample.demo.model;
 
 import lombok.*;
 
-@Getter 3 usages
-
+@Getter
 @Setter
-
 @NoArgsConstructor
-
-@LLArgsConstructor
-
+@ALLArgsConstructor
 @Builder
-
+@Entity
+@Validated
+@Valid
+@Table(name "user_table_testing")
 public class UserDetails {
 
-private int id;
+@Id
+@Min(value = 1)
+@Max(value = 100)
+private Integer id;
+
+@Size(min=1, max 100, message "first name is not valid please enter min 1 and max 100 letter")
+@Column(name="first_name")
+@Pattern (regexp="[A-Za-z0-9]-8")
 
 private String firstName;
 
+@Column(name = "Last_name")
+@size(min 0, max 100, messago "the last name is not valid")
+@NotNull
+
 private String lastName;
 
-private String mobile;
+@Column(name "age")
+@min (value=1)
+@Max(value= 100)
+private Integer age;
 
-private String email;
+@NotNull
+
+@NotEmpty
+
+private List<String> usero;
 
 }
